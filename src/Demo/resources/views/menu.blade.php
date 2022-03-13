41f5d3e9ae8f54e423729ca372dd7f39cd17b914
felice <felice.ostuni@uania.com>
2022-03-13 20:29:04 +0100

    wip

--- src/Demo/resources/views/menu.blade.php
+++ src/Demo/resources/views/menu.blade.php
@@ -10,7 +10,7 @@
                 <li class="nav-item"><a class="nav-link {{ Request::is('rapyd-demo') ? 'active' : '' }}" href="{{ route("rapyd.demo", "Home") }}"> Home </a></li>
 {{--                <li class="nav-item"><a class="nav-link {{ Request::is('rapyd-demo/users*') ? 'active' : '' }}" href="{{ route("rapyd.demo.users", "Users") }}"> Users </a></li>--}}
                 <li class="nav-item"><a class="nav-link {{ Request::is('rapyd-demo/articles*') ? 'active' : '' }}" href="{{ route("rapyd.demo.articles") }}"> DataTable </a></li>
-                <li class="nav-item"><a class="nav-link {{ Request::is('rapyd-demo/article/*') ? 'active' : '' }}" href="{{ route("rapyd.demo.articles.edit", ['article'=>1]) }}"> DataTable </a></li>
+                <li class="nav-item"><a class="nav-link {{ Request::is('rapyd-demo/article/*') ? 'active' : '' }}" href="{{ route("rapyd.demo.articles.edit", ['article'=>1]) }}"> DataEdit </a></li>
             </ul>
 
 
